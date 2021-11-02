511ec23a8c26115b7fa670a49564d3dbd01d3f94
LabuzovaMariaya <myubykova@yandex.ru>
2021-11-02 13:35:42 +0300

    Add Task1

---
+++ Task1/Program.cs
@@ -0,0 +1,48 @@
+﻿void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(1,15);       
+        index++;
+    }
+
+}
+
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.WriteLine(col[position]);
+        position++;
+    }
+}
+int SumN(int[] collection, int n, int k, int l)
+{
+    int count = collection.Length;
+    int index = 0;
+    int sum = 0;
+    
+    while (index < count)
+    {
+        // if (collection[index] < n && (collection[index] % k == 0 | collection[index] % l == 0 ))
+        if (collection[index] < n && !(collection[index] % (k+l) == 0))
+        {
+            sum++;
+        }
+        index++;
+    }
+    return sum;
+}
+
+int[] array = new int[10];
+
+FillArray(array);
+PrintArray(array);
+Console.WriteLine();
+
+int pos = SumN(array, 13, 2, 4);
+Console.WriteLine(pos);
